cda63b98aae7c2216aebfa503ac0e6a958d1ada7
Johan Andrés Villada <vvj1998@protonmail.com>
2025-03-12 17:13:56 -0500

    Se agrego la clase Persona

--- src/autonoma/biblioteca/models/Persona.java
+++ src/autonoma/biblioteca/models/Persona.java
@@ -6,8 +6,63 @@ package autonoma.biblioteca.models;
 
 /**
  *
- * @author PABLO VI
+ * Modelo que permite representar una persona
+ * 
+ * @author Johan Andrés Villada Valencia
+ * @since 20250311
+ * @version 1.0.0
+ * 
  */
 public class Persona {
+    ////////////////////////////////////////////////////////////////////
+    ///// Atributos
+    
+    /**
+     * Nombre de la persona
+     */
+    private String nombre;
+    
+    /**
+     * Documento de la persona
+     */
+    private String documentoIdentidad;
+    
+    
+    /*
+    Correo de la persona
+    */
+    private String correoElectronico;
+
+    
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDocumentoIdentidad() {
+        return documentoIdentidad;
+    }
+
+    public void setDocumentoIdentidad(String documentoIdentidad) {
+        this.documentoIdentidad = documentoIdentidad;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+    
+    
+    
+    
     
 }
+
+
